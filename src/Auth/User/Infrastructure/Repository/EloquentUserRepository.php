4011a0e248703943971bf04fa64eae4f9f6d83ff
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2024-10-21 12:21:53 -0500

    fix(auth): actualiza generacion de ids al crear usuarios

--- src/Auth/User/Infrastructure/Repository/EloquentUserRepository.php
+++ src/Auth/User/Infrastructure/Repository/EloquentUserRepository.php
@@ -79,7 +79,9 @@ class EloquentUserRepository implements UserRepository
 
     public function create($data)
     {
-        $id = $this->builder()->count() + 1;
+        // $id = $this->builder()->count() + 1;
+        $id = $this->builder()->select(DB::raw("max(id) + 1 as next_id"))->first()->next_id;
+
         $this->builder()->insert([
             'id' => $id,
             'username' => $data['username'],
