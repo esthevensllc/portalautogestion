68e7ee675cdeb2a73ce1c11146f5580b348d234a
Diego Moreno | Sol 170053 <C19884@limnwkdaswfv01.claro.pe>
2024-12-16 10:10:58 -0500

    se cambio metodo get a post para DAPU

--- routes/web.php
+++ routes/web.php
@@ -172,13 +172,13 @@ Route::group([
     });
     Route::group(['prefix' => 'dapu/suspension-servicio', 'trac_name' => 'dapu.suspension-servicio'], function(){
         Route::get('/', [\AMovil\Reports\DAPU\SuspensionServicio\Controllers\SuspensionServicioController::class, 'view']);
-        Route::post('/json', [\AMovil\Reports\DAPU\SuspensionServicio\Controllers\SuspensionServicioController::class, 'getData']);
-        Route::post('/export', [\AMovil\Reports\DAPU\SuspensionServicio\Controllers\SuspensionServicioController::class, 'export']);
+        Route::get('/json', [\AMovil\Reports\DAPU\SuspensionServicio\Controllers\SuspensionServicioController::class, 'getData']);
+        Route::get('/export', [\AMovil\Reports\DAPU\SuspensionServicio\Controllers\SuspensionServicioController::class, 'export']);
     });
     Route::group(['prefix' => 'dapu/adquisicion', 'trac_name' => 'dapu.adquisicion'], function(){
         Route::get('/', [\AMovil\Reports\DAPU\Adquisiciones\Controllers\AdquisicionEquipoController::class, 'view']);
-        Route::get('json', [\AMovil\Reports\DAPU\Adquisiciones\Controllers\AdquisicionEquipoController::class, 'getData']);
-        Route::get('export', [\AMovil\Reports\DAPU\Adquisiciones\Controllers\AdquisicionEquipoController::class, 'export']);
+        Route::post('json', [\AMovil\Reports\DAPU\Adquisiciones\Controllers\AdquisicionEquipoController::class, 'getData']);
+        Route::post('export', [\AMovil\Reports\DAPU\Adquisiciones\Controllers\AdquisicionEquipoController::class, 'export']);
     });
     Route::group(['prefix' => 'dapu/consulta-linea', 'trac_name' => 'dapu.consulta-linea'], function(){
         Route::get('/', [\AMovil\Reports\DAPU\Lineas\Controllers\LineaController::class, 'view']);
