46a91e95c0d46c9feab35e43d660e1ac5a893b88
Diego Moreno | Sol 170053 <C19884@limnwkdaswfv01.claro.pe>
2024-12-16 10:07:55 -0500

    se cambio route para dapu de get a post

--- resources/views/dapu/adquisicion.blade.php
+++ resources/views/dapu/adquisicion.blade.php
@@ -97,7 +97,7 @@
         try {
             const formData = new FormData(e.target);
             const response = await fetch(`${config.url}`, {
-                method: 'GET',
+                method: 'POST',
                 headers: {
                     'Accept': 'application/json'
                 },
@@ -145,7 +145,7 @@
             formData.append('type', type);
 
             fetch("{{ asset('dapu/adquisicion/export') }}", {
-                method: 'GET',
+                method: 'POST',
                 headers: {
                     'Accept': 'application/json'
                 },
