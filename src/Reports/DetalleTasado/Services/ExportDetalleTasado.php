c3b08ce7d9b0b570b5754f49f6179e8b6a29576c
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-03-26 15:46:33 -0500

    feat(detalletasado): agregar captura de input en log de reportes

--- src/Reports/DetalleTasado/Services/ExportDetalleTasado.php
+++ src/Reports/DetalleTasado/Services/ExportDetalleTasado.php
@@ -3,6 +3,9 @@
 namespace AMovil\Reports\DetalleTasado\Services;
 
 use AMovil\Reports\DetalleTasado\Domain\DetalleTasadoRepository;
+use AMovil\Reports\ReportLog\Domain\ReportLogStatus;
+use AMovil\Reports\ReportLog\Services\SaveReportDto;
+use AMovil\Reports\ReportLog\Services\SaveReportLog;
 use AMovil\Shared\Application\Response;
 use AMovil\Shared\Exports\Domain\ExportService;
 use AMovil\Shared\Exports\Domain\WriterType;
@@ -13,61 +16,96 @@ class ExportDetalleTasado
 {
     private $repo;
     private $exportService;
+    private $saveReportLog;
 
-    public function __construct(DetalleTasadoRepository $repo, ExportService $exportService)
+    public function __construct(DetalleTasadoRepository $repo, ExportService $exportService, SaveReportLog $saveReportLog)
     {
         $this->repo = $repo;
         $this->exportService = $exportService;
+        $this->saveReportLog = $saveReportLog;
     }
 
     public function __invoke($numerosCuenta, $tipoInput, $fecha, $fechaIni, $fechaFin): Response
     {
-        $dtFechaIni = null;
-        $dtFechaFin = null;
-        if($tipoInput === "1"){
-            $dtFechaIni = DateTime::createFromFormat("Y-m-d", $fecha);
-            $dtFechaFin = DateTime::createFromFormat("Y-m-d", $fecha);
-        }else{
-            $dtFechaIni = DateTime::createFromFormat("Y-m-d", $fechaIni);
-            $dtFechaFin = DateTime::createFromFormat("Y-m-d", $fechaFin);
-        }
+        $fechaIniExec = new DateTime();
+        $reporteInput = ['tipoInput' => $tipoInput, 'numerosCuenta' => $numerosCuenta];
+        try {
+            $dtFechaIni = null;
+            $dtFechaFin = null;
+            if($tipoInput === "1"){
+                $dtFechaIni = DateTime::createFromFormat("Y-m-d", $fecha);
+                $dtFechaFin = DateTime::createFromFormat("Y-m-d", $fecha);
+            }else{
+                $dtFechaIni = DateTime::createFromFormat("Y-m-d", $fechaIni);
+                $dtFechaFin = DateTime::createFromFormat("Y-m-d", $fechaFin);
+            }
+            $reporteInput['fechaInicio'] = $dtFechaIni->format("Y-m-d");
+            $reporteInput['fechaFin'] = $dtFechaFin->format("Y-m-d");
 
-        $data = $this->repo->getReporte(explode(",", str_replace(" ", "", $numerosCuenta)), $dtFechaIni, $dtFechaFin);
+            $data = $this->repo->getReporte(explode(",", str_replace(" ", "", $numerosCuenta)), $dtFechaIni, $dtFechaFin);
 
-        $options = [
-            "sheetIndex" => 0,
-            'title' => "DETALLE TASADO",
-            'styles' => [
-                'header' => [
-                    'font' => ['bold' => true, 'size' => 9],
-                    'borders'=> [
-                        'allBorders' => [
-                            'borderStyle' => Border::BORDER_THIN,
-                            'color' => array('rgb'=>'000000')
+            $options = [
+                "sheetIndex" => 0,
+                'title' => "DETALLE TASADO",
+                'styles' => [
+                    'header' => [
+                        'font' => ['bold' => true, 'size' => 9],
+                        'borders'=> [
+                            'allBorders' => [
+                                'borderStyle' => Border::BORDER_THIN,
+                                'color' => array('rgb'=>'000000')
+                            ]
                         ]
+                    ],
+                    'body' => [
+                        'font' => ['size' => 9]
                     ]
-                ],
-                'body' => [
-                    'font' => ['size' => 9]
                 ]
-            ]
-        ];
+            ];
 
-        $headers = [
-            "fch_trafico" => ["label" => "FCH_TRAFICO"],
-            "msisdn" => ["label" => "MSISDN"],
-            "plan" => ["label" => "PLAN"],
-            "servicio_des" => ["label" => "SERVICIO_DES"],
-            "trafico_bytes" => ["label" => "TRAFICO_BYTES"],
-        ];
+            $headers = [
+                "fch_trafico" => ["label" => "FCH_TRAFICO"],
+                "msisdn" => ["label" => "MSISDN"],
+                "plan" => ["label" => "PLAN"],
+                "servicio_des" => ["label" => "SERVICIO_DES"],
+                "trafico_bytes" => ["label" => "TRAFICO_BYTES"],
+            ];
 
-        $this->exportService->loadData($headers, $data, $options);
-        $exportContent = $this->exportService->getWriter(WriterType::XLSX)->getOutput();
+            $this->exportService->loadData($headers, $data, $options);
 
-        return new Response([], [
-            "filename" => "DETALLE_TASADO.xlsx",
-            "type" => "xlsx",
-            "content" => $exportContent,
-        ]);
+            $filename = "DETALLE_TASADO_".$fechaIniExec->format('YmdHis').".xlsx";
+            $filePath = SaveReportLog::LOCAL_PATH."/DETALLE_TASADO/{$filename}";
+
+            $this->exportService->getWriter(WriterType::XLSX)->save($filePath);
+
+            $this->reportLog($filePath, $filename, $fechaIniExec, new DateTime(), $reporteInput, null);
+
+            return new Response([], [
+                "filename" => $filename,
+                "type" => "xlsx",
+                "content" => file_get_contents($filePath),
+            ]);
+        } catch (\Throwable $th) {
+            $this->reportLog(null, null, $fechaIniExec, new DateTime(), $reporteInput, $th);
+            throw $th;
+        }
+    }
+
+    private function reportLog(?string $local_file, ?string $filename, DateTime $ini, DateTime $fin, array $input, ?\Throwable $error)
+    {
+        $logDto = SaveReportDto::create(
+            'DETALLE_TASADO',
+            $filename,
+            $ini,
+            $fin,
+            $error === null ? ReportLogStatus::CORRECTO : ReportLogStatus::ERROR,
+            $error !== null ? $error->getMessage() : null,
+            'rep-det-consumo.detalle-tasado',
+            json_encode($input)
+        );
+        $this->saveReportLog->create($logDto);
+        if ($local_file !== null) {
+            $this->saveReportLog->sendFileToRemoteServer($local_file, "DETALLE_TASADO/{$filename}");
+        }
     }
 }
