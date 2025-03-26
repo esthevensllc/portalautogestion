b091e1ee6a3147e9da78fc7f10c2b088655ad0b2
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-03-26 17:00:06 -0500

    feat(geomarketing): agregar captura de input en log de reportes

--- src/Reports/GeoMarketing/Services/ExportGeoMarketing.php
+++ src/Reports/GeoMarketing/Services/ExportGeoMarketing.php
@@ -3,7 +3,12 @@
 namespace AMovil\Reports\GeoMarketing\Services;
 
 use AMovil\Reports\GeoMarketing\Domain\GeoMarketingRepository;
+use AMovil\Reports\ReportLog\Domain\ReportLogStatus;
+use AMovil\Reports\ReportLog\Services\SaveReportDto;
+use AMovil\Reports\ReportLog\Services\SaveReportLog;
 use AMovil\Shared\Application\Response;
+use AMovil\Shared\FileStorage\Domain\StorageService;
+use AMovil\Shared\FileStorage\Domain\StorageSystemName;
 use DateInterval;
 use DateTime;
 use Exception;
@@ -24,54 +29,87 @@ class ExportGeoMarketing
         "11" => ["name" => "GRAN_TEATRO_NACIONAL"],
     ];
     private $repository;
+    private $saveReportLog;
+    private $storage;
     const MAX_DAYS = 30;
 
-    public function __construct(GeoMarketingRepository $repo)
+    public function __construct(GeoMarketingRepository $repo, SaveReportLog $saveReportLog, StorageService $storage)
     {
         $this->repository = $repo;
+        $this->saveReportLog = $saveReportLog;
+        $this->storage = $storage->getStorageSystemByName(StorageSystemName::LOCAL2);
     }
 
     public function __invoke($nintex, $base, $fechaIni, $fechaFin, $whiteList)
     {
-        $dtFechaIni = DateTime::createFromFormat("Y-m-d H:i", $fechaIni);
-        $dtFechaFin = DateTime::createFromFormat("Y-m-d H:i", $fechaFin);
-        $now = new DateTime();
-        $diff = $dtFechaFin->getTimestamp() - $dtFechaIni->getTimestamp();
-        $maxDays = self::MAX_DAYS;
-        $max_diff_hours = $maxDays*24*3600;
-        if($max_diff_hours < $diff){
-            throw new Exception("No se puede consultar un rango mayor a {$maxDays} dias");
+        $fechaIniExec = new DateTime();
+        $reporteInput = ["nintex" => $nintex, 'base' => $base, 'fechaInicio' => $fechaIni, 'fechaFin' => $fechaFin, 'whiteList' => $whiteList];
+
+        try {
+            $dtFechaIni = DateTime::createFromFormat("Y-m-d H:i", $fechaIni);
+            $dtFechaFin = DateTime::createFromFormat("Y-m-d H:i", $fechaFin);
+            $now = new DateTime();
+            $diff = $dtFechaFin->getTimestamp() - $dtFechaIni->getTimestamp();
+            $maxDays = self::MAX_DAYS;
+            $max_diff_hours = $maxDays*24*3600;
+            if($max_diff_hours < $diff){
+                throw new Exception("No se puede consultar un rango mayor a {$maxDays} dias");
+            }
+            $minDateTime = new DateTime();
+            $minDateTime->modify("-{$maxDays} day");
+            if($minDateTime->format("Ymd") > $dtFechaIni->format("Ymd")){
+                throw new Exception("No se puede consultar un rango mayor a {$maxDays} dias");
+            }
+            if($dtFechaFin->format("Y-m-d") > $now->format("Y-m-d")){
+                throw new Exception("No se puede consultar una fecha superior al dia actual");
+            }
+
+            $data = $this->repository->getReport($base, $dtFechaIni, $dtFechaFin, $whiteList);
+
+            $content = "";
+            $content .= "MSISDN".PHP_EOL;
+            foreach($data as $row){
+                $content .= $row["msisdn"].PHP_EOL;
+            }
+
+            $baseName = $this->baseById[$base]["name"];
+
+            $now = new DateTime();
+            $strNow = $now->format("YmdHis");
+            $whiteListSubName = (int) $whiteList === 1 ? "WHITE_LIST_" : "";
+            $filename = "BASE_{$baseName}_{$whiteListSubName}{$strNow}.csv";
+
+            $this->repository->saveLog($nintex, $base, $dtFechaIni, $dtFechaFin, $now, $filename, $whiteList);
+
+            $this->storage->put(SaveReportLog::LOCAL_PATH."/GEOMARKETING/{$filename}", $content);
+            $this->reportLog(SaveReportLog::LOCAL_PATH."/GEOMARKETING/{$filename}", $filename, $fechaIniExec, new DateTime(), $reporteInput, null);
+
+            return new Response([], [
+                "filename" => $filename,
+                "type" => "csv",
+                "content" => $content,
+            ]);
+        } catch (\Throwable $th) {
+            $this->reportLog(null, null, $fechaIniExec, new DateTime(), $reporteInput, $th);
+            throw $th;
         }
-        $minDateTime = new DateTime();
-        $minDateTime->modify("-{$maxDays} day");
-        if($minDateTime->format("Ymd") > $dtFechaIni->format("Ymd")){
-            throw new Exception("No se puede consultar un rango mayor a {$maxDays} dias");
+    }
+
+    private function reportLog(?string $local_file, ?string $filename, DateTime $ini, DateTime $fin, array $input, ?\Throwable $error)
+    {
+        $logDto = SaveReportDto::create(
+            'GEOMARKETING',
+            $filename,
+            $ini,
+            $fin,
+            $error === null ? ReportLogStatus::CORRECTO : ReportLogStatus::ERROR,
+            $error !== null ? $error->getMessage() : null,
+            'geomarketing',
+            json_encode($input)
+        );
+        $this->saveReportLog->create($logDto);
+        if ($local_file !== null) {
+            $this->saveReportLog->sendFileToRemoteServer($local_file, "GEOMARKETING/{$filename}");
         }
-        if($dtFechaFin->format("Y-m-d") > $now->format("Y-m-d")){
-            throw new Exception("No se puede consultar una fecha superior al dia actual");
-        }
-
-        $data = $this->repository->getReport($base, $dtFechaIni, $dtFechaFin, $whiteList);
-
-        $content = "";
-        $content .= "MSISDN".PHP_EOL;
-        foreach($data as $row){
-            $content .= $row["msisdn"].PHP_EOL;
-        }
-
-        $baseName = $this->baseById[$base]["name"];
-
-        $now = new DateTime();
-        $strNow = $now->format("YmdHis");
-        $whiteListSubName = (int) $whiteList === 1 ? "WHITE_LIST_" : "";
-        $filename = "BASE_{$baseName}_{$whiteListSubName}{$strNow}.csv";
-
-        $this->repository->saveLog($nintex, $base, $dtFechaIni, $dtFechaFin, $now, $filename, $whiteList);
-
-        return new Response([], [
-            "filename" => $filename,
-            "type" => "csv",
-            "content" => $content,
-        ]);
     }
 }
