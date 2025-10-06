39435f7ba146a11a70618e69b11db86ab8c5b206
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-10-06 18:52:11 -0500

    fix(dapu): agregar modulo busqueda sim card

---
+++ resources/views/dapu/busqueda_sim_card_form.blade.php
@@ -0,0 +1,9 @@
+<div class="col-lg-3 col-md-4">
+    <div class="form-group">
+        <label for="">ICCID</label>
+        <input type="number" name="iccid" class="form-control form-control-sm" required>
+        <div class="invalid-feedback d-block text-dark">
+            <br>Ej: 895110113001103494
+        </div>
+    </div>
+</div>
