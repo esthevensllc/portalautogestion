c9e98961b20d9c3342171a82997ff05a2eb8bad2
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-10-09 11:47:48 -0500

    chore(dapu): actualizar query ultimo trafico

--- src/Reports/DAPU/UltimoTrafico/Infrastructure/EloquentUltimoTraficoRepository.php
+++ src/Reports/DAPU/UltimoTrafico/Infrastructure/EloquentUltimoTraficoRepository.php
@@ -39,26 +39,72 @@ class EloquentUltimoTraficoRepository implements UltimoTraficoRepository
             DB::table("USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier}")->insert(["value" => $value]);
         }
 
-        $queryFilter = null;
+        // $queryFilter = null;
         if($field === "msisdn"){
-            $queryFilter = "T.MSISDN IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})";
-        } else {
-            $queryFilter = "T.IMEI IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})";
-        }
+            $query = "SELECT MSISDN, IMSI, null IMEI, FEC_ULTIMO_TRAFICO FROM (
+                SELECT  /*+ PARALLEL(8) */ 
+                    T.MSISDN,
+                    T.IMSI,
+                    T.FEC_ULTIMO_TRAFICO,
+                    CASE 
+                        WHEN T.SOURCE_TABLE = 'F_M_TRIPLETA' THEN 'VOZ'
+                        WHEN T.SOURCE_TABLE = 'F_M_TRIPLETA_GPRS' THEN 'DATOS'
+                    END AS FUENTE,
+                    ROW_NUMBER() OVER (PARTITION BY T.MSISDN ORDER BY T.FEC_ULTIMO_TRAFICO DESC) AS RN
+                FROM (
+                    SELECT /*+ PARALLEL(8) */ 
+                        MSISDN,
+                        IMSI,
+                        FEC_ULTIMO_TRAFICO,
+                        'F_M_TRIPLETA' AS SOURCE_TABLE
+                    FROM DWA.F_M_TRIPLETA
+                    WHERE MSISDN IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})
 
-        $query = "SELECT
-            MSISDN, IMSI, IMEI, FEC_ULTIMO_TRAFICO, RN
-        FROM (
-            SELECT T.MSISDN, T.IMSI, SUBSTR(T.IMEI, 0, 14) AS IMEI, T.FEC_ULTIMO_TRAFICO,
-                ROW_NUMBER() OVER (PARTITION BY T.MSISDN ORDER BY T.FEC_ULTIMO_TRAFICO DESC) AS RN
-            FROM (
-                SELECT /*+ PARALLEL(20) */ T.MSISDN, T.IMSI, T.IMEI, T.FEC_ULTIMO_TRAFICO
-                FROM DWA.F_M_TRIPLETA T WHERE {$queryFilter}
-                UNION ALL
-                SELECT /*+ PARALLEL(20) */ T.MSISDN, T.IMSI, T.IMEI, T.FEC_ULTIMO_TRAFICO
-                FROM DWA.F_M_TRIPLETA_GPRS T WHERE {$queryFilter}
-            ) T
-        ) WHERE RN = 1";
+                    UNION ALL
+
+                    SELECT /*+ PARALLEL(8) */ 
+                        MSISDN,
+                        IMSI,
+                        FEC_ULTIMO_TRAFICO,
+                        'F_M_TRIPLETA_GPRS' AS SOURCE_TABLE
+                    FROM DWA.F_M_TRIPLETA_GPRS
+                    WHERE MSISDN IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})
+                ) T
+            )
+            WHERE RN = 1";
+        } else {
+            $query = "SELECT null MSISDN, IMSI, IMEI, FEC_ULTIMO_TRAFICO FROM (
+                SELECT /*+ PARALLEL(8) */  
+                    T.IMEI,
+                    T.IMSI,
+                    T.FEC_ULTIMO_TRAFICO,
+                    CASE 
+                        WHEN T.SOURCE_TABLE = 'F_M_TRIPLETA' THEN 'VOZ'
+                        WHEN T.SOURCE_TABLE = 'F_M_TRIPLETA_GPRS' THEN 'DATOS'
+                    END AS FUENTE,
+                    ROW_NUMBER() OVER (PARTITION BY T.IMEI ORDER BY T.FEC_ULTIMO_TRAFICO DESC) AS RN
+                FROM (
+                    SELECT /*+ PARALLEL(8) */ 
+                        IMEI,
+                        IMSI,
+                        FEC_ULTIMO_TRAFICO,
+                        'F_M_TRIPLETA' AS SOURCE_TABLE
+                    FROM DWA.F_M_TRIPLETA
+                    WHERE IMEI IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})
+
+                    UNION ALL
+
+                    SELECT /*+ PARALLEL(8) */ 
+                        SUBSTR(IMEI,1,15) IMEI,
+                        IMSI,
+                        FEC_ULTIMO_TRAFICO,
+                        'F_M_TRIPLETA_GPRS' AS SOURCE_TABLE
+                    FROM DWA.F_M_TRIPLETA_GPRS
+                    WHERE SUBSTR(IMEI,1,15) IN (SELECT value FROM USRAES.DAPU_LINEA_IMEI_INPUT_{$this->userIdentifier})
+                ) T
+            )
+            WHERE RN = 1";
+        }
 
         $data = DB::select(DB::raw($query));
 
