9afb1f5536c4085c78c6b0465a3a230f2e90fa94
Yordan Rojas <yordan.rojas@claro.com.pe>
2026-04-08 10:07:43 -0500

    cambio por Orlando

--- resources/views/vendor/backpack/base/rep_det_consumo/detalle_llamadas.blade.php
+++ resources/views/vendor/backpack/base/rep_det_consumo/detalle_llamadas.blade.php
@@ -71,7 +71,7 @@
             </div>
             <div class="text-danger">
                 *Toda consulta que se realice se registrara en un log
-                <br>*Si el periodo a consultar no se entra en el rango mostrado comunicarse con el área correspondiente
+                <br>*Si el periodo a consultar no se encuentra en el rango mostrado comunicarse con el área correspondiente
                 <br>*El equipo de facturación cuenta con 5 dias de plazo luego del ciclo de cierre para cargar información del recibo
             </div>
         </form>
